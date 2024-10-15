783360063e6cc1f027398295d5402f11ede8bbf4
Faisal Karim Chowdhury <faisalchy64@gmail.com>
2024-10-16 00:16:36 +0600

    feat: Create ProductLoader.jsx component

---
+++ src/uis/ProductLoader.jsx
@@ -0,0 +1,77 @@
+export default function ProductLoader() {
+  return (
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3.5">
+      <article className="border rounded-md overflow-hidden">
+        <div className="h-60 bg-gray-300 animate-pulse"></div>
+        <div className="p-3.5">
+          <h3 className="h-8 bg-gray-300 animate-pulse"></h3>
+          <p className="h-14 bg-gray-300 animate-pulse my-2.5"></p>
+          <div className="flex items-center justify-between">
+            <h3 className="w-14 h-7 bg-gray-300 animate-pulse"></h3>
+            <button className="w-28 h-10 bg-gray-300 animate-pulse"></button>
+          </div>
+        </div>
+      </article>
+
+      <article className="border rounded-md overflow-hidden">
+        <div className="h-60 bg-gray-300 animate-pulse"></div>
+        <div className="p-3.5">
+          <h3 className="h-8 bg-gray-300 animate-pulse"></h3>
+          <p className="h-14 bg-gray-300 animate-pulse my-2.5"></p>
+          <div className="flex items-center justify-between">
+            <h3 className="w-14 h-7 bg-gray-300 animate-pulse"></h3>
+            <button className="w-28 h-10 bg-gray-300 animate-pulse"></button>
+          </div>
+        </div>
+      </article>
+
+      <article className="border rounded-md overflow-hidden">
+        <div className="h-60 bg-gray-300 animate-pulse"></div>
+        <div className="p-3.5">
+          <h3 className="h-8 bg-gray-300 animate-pulse"></h3>
+          <p className="h-14 bg-gray-300 animate-pulse my-2.5"></p>
+          <div className="flex items-center justify-between">
+            <h3 className="w-14 h-7 bg-gray-300 animate-pulse"></h3>
+            <button className="w-28 h-10 bg-gray-300 animate-pulse"></button>
+          </div>
+        </div>
+      </article>
+
+      <article className="border rounded-md overflow-hidden">
+        <div className="h-60 bg-gray-300 animate-pulse"></div>
+        <div className="p-3.5">
+          <h3 className="h-8 bg-gray-300 animate-pulse"></h3>
+          <p className="h-14 bg-gray-300 animate-pulse my-2.5"></p>
+          <div className="flex items-center justify-between">
+            <h3 className="w-14 h-7 bg-gray-300 animate-pulse"></h3>
+            <button className="w-28 h-10 bg-gray-300 animate-pulse"></button>
+          </div>
+        </div>
+      </article>
+
+      <article className="border rounded-md overflow-hidden">
+        <div className="h-60 bg-gray-300 animate-pulse"></div>
+        <div className="p-3.5">
+          <h3 className="h-8 bg-gray-300 animate-pulse"></h3>
+          <p className="h-14 bg-gray-300 animate-pulse my-2.5"></p>
+          <div className="flex items-center justify-between">
+            <h3 className="w-14 h-7 bg-gray-300 animate-pulse"></h3>
+            <button className="w-28 h-10 bg-gray-300 animate-pulse"></button>
+          </div>
+        </div>
+      </article>
+
+      <article className="border rounded-md overflow-hidden">
+        <div className="h-60 bg-gray-300 animate-pulse"></div>
+        <div className="p-3.5">
+          <h3 className="h-8 bg-gray-300 animate-pulse"></h3>
+          <p className="h-14 bg-gray-300 animate-pulse my-2.5"></p>
+          <div className="flex items-center justify-between">
+            <h3 className="w-14 h-7 bg-gray-300 animate-pulse"></h3>
+            <button className="w-28 h-10 bg-gray-300 animate-pulse"></button>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+}
