f0f15c802e889e3aa0f097e9145a4ab793995fe1
Faisal Karim Chowdhury <faisalchy64@gmail.com>
2024-10-16 18:00:24 +0600

    feat: Add fetch products functionality from api to Container.jsx and Card.jsx components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,35 @@
 import { Link } from "react-router-dom";
 
-export default function Card() {
+export default function Card({ product }) {
+  const {
+    _id,
+    productName,
+    productDescription,
+    productPrice,
+    productMeasure,
+    productImage,
+  } = product;
+
   return (
     <article className="bg-gray-100 border rounded-md overflow-hidden">
       <div className="h-60">
         <img
-          src="https://images.pexels.com/photos/27835751/pexels-photo-27835751/free-photo-of-a-tree-with-green-apples-on-it.jpeg?auto=compress&w=600"
-          alt="Product image"
+          src={productImage}
+          alt={productName}
           className="w-full h-full object-cover"
         />
       </div>
       <div className="p-3.5">
-        <h3 className="text-2xl font-semibold text-gray-800">Apple</h3>
-        <p className="text-sm text-gray-500 py-2.5">
-          Fresh and organic apple. Fresh and organic apple. Fresh and organic
-          apple.
-        </p>
+        <h3 className="text-2xl font-semibold capitalize text-gray-800">
+          {productName}
+        </h3>
+        <p className="text-sm text-gray-500 py-2.5">{productDescription}</p>
         <div className="flex items-center justify-between mt-2.5">
-          <h2 className="text-xl font-semibold text-gray-700">$5.00</h2>
+          <h2 className="text-xl font-semibold uppercase text-gray-700">
+            ${productPrice} {productMeasure}
+          </h2>
           <Link
-            to={`/products/${1}`}
+            to={`/products/${_id}`}
             className="text-sm text-white bg-gray-900 hover:bg-gray-800 px-3.5 py-2.5 rounded-2xl"
           >
             Order Now
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,20 +1,50 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import api from "../api";
+import ProductLoader from "../uis/ProductLoader";
 import Card from "./Card";
 
 export default function Container() {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        setLoading(true);
+        const res = await api.get("/products");
+        setProducts(res.data.products);
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    };
+
+    getProducts();
+    setLoading(false);
+  }, []);
+
   return (
     <section className="w-4/5 min-h-[500px] flex flex-col gap-10 py-10 mx-auto">
       <h1 className="text-2xl md:text-3xl font-semibold text-gray-900">
         Browse, compare and order products from trusted suppliers
       </h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3.5">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+
+      {loading && <ProductLoader />}
+
+      {products.length > 0 && (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3.5">
+          {products.map((product) => (
+            <Card key={product._id} product={product} />
+          ))}
+        </div>
+      )}
+
+      {products.length === 0 && (
+        <div className="h-80 flex items-center justify-center">
+          <h3 className="text-gray-500">No products found.</h3>
+        </div>
+      )}
 
       <Link
         to="/products"
